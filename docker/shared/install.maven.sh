866fad5fac5a020b443b8f6395957a84abcabda6
Stoyan Demirev <stoyan.demirev@careem.com>
2020-12-24 10:12:50 +0100

    fix linux docker build: fix regex, use maven-3.5.4

--- docker/shared/install.maven.sh
+++ docker/shared/install.maven.sh
@@ -1,6 +1,6 @@
 
 echo "Preparing Maven..."
-curl http://www-eu.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz -O
+curl http://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz -OL
 mkdir -p /opt
-tar xzvf apache-maven-3.5.0-bin.tar.gz -C /opt/
-chmod -R 777 /opt/apache-maven-3.5.0
+tar xzvf apache-maven-3.5.4-bin.tar.gz -C /opt/
+chmod -R 777 /opt/apache-maven-3.5.4
